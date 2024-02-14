ab21636a1071f9c224b53749e666b3a03e76d559
fran98m <fj.montalvo@uniandes.edu.co>
2024-02-14 15:30:54 -0500

    V2.0

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.core.embeddings import resolve_embed_model
-from llama_index.llns.ollama import Ollama
+from llama_index.llms.ollama import Ollama
 
 documents=SimpleDirectoryReader('data')
 
-import logger
+import logging
 
 #Logger Config
 
@@ -25,15 +25,32 @@ logger.addHandler(stream_handler)
 
 
 try:
-    llm=Ollama(model='dolphin-mixtral',
-    callback_manager=CallbackManager([StreamingStdoutCallback()]),
-    temperature=0,
-    max_tokens=256)
-    logger.info(f"Modelo cargado estos son los parametros del modelo: temperature={llm.temperature}, max_tokens={llm.max_tokens}")
-
+    documents = SimpleDirectoryReader('data').load_data()
+    logger.info('Data loaded successfully')
 except Exception as e:
-    logger.error(f"Error {e} al cargar el modelo/error loading model",exec_info=True)
+    logger.error('Error loading data: {e}', exc_info=True)
 
-#Para enviar un prompt al modelo simple/To send a prompt to the simple model
-#llm("Prompt")
+try:
+    Settings.embed_model = resolve_embed_model('local:BAII/bge-small-en-v1.5')
+    logger.info('Embed model loaded successfully')
+except Exception as e:
+    logger.error(f'Error loading embed model: {e}', exc_info=True)
 
+try:
+    Settings.llm=Ollama(model='dolphin-mixtral',request_timeout=40)
+    logger.info('Ollama model loaded successfully')
+except Exception as e:
+    logger.error(f'Error loading Ollama model: {e}', exc_info=True)
+
+try:
+    index=VectorStoreIndex.from_documents(documents,)
+    logger.info('Index loaded successfully')
+except Exception as e:
+    logger.error(f'Error loading index: {e}', exc_info=True)
+
+try:
+    query_engine=index.as_query_engine()
+    response=('What thesis does the author proposes and what arguments does he gives?')
+    logger.info(response)
+except Exception as e:
+    logger.error(f'Error loading query engine: {e}', exc_info=True)
